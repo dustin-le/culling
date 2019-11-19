ed489b2bd2b00237a9ebdd3f0423dc473c14676f
Dustin Le <ledustin2@gmail.com>
2019-11-19 00:02:26 -0600

    Initial commit

---
+++ bezier.h
@@ -0,0 +1,19 @@
+// Le, Dustin E.
+// dxl0689
+// 2019-11-22
+
+#if !defined( __BEZIER_H__ )
+#define __BEZIER_H__
+
+//----------------------------------------------------------
+#include "model.h"
+#include "projection.h"
+#include "view.h"
+
+//----------------------------------------------------------
+void generateBezierPoints( Model *model, int resolution );
+void generateBezierTriangles( Model *model, View *view, Projection *projection );
+
+//----------------------------------------------------------
+#endif
+
---
+++ triangle.h
@@ -0,0 +1,19 @@
+// Le, Dustin E.
+// dxl0689
+// 2019-11-22
+
+#if !defined( __TRIANGLE_H__ )
+#define __TRIANGLE_H__
+
+//----------------------------------------------------------
+#include "vertex.h"
+#include "view.h"
+
+//----------------------------------------------------------
+void cullProjectDumpTriangle( View *view, Projection *projection, Vertex *v1, Vertex *v2, Vertex *v3 );
+void dumpTriangle( Vertex *v1, Vertex *v2, Vertex *v3 );
+void setPortal( View *v );
+
+//----------------------------------------------------------
+#endif
+

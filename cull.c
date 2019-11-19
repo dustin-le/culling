2ecca33f2d049a078669c4bbfc7a53fe7ef82b8a
Dustin Le <ledustin2@gmail.com>
2019-11-19 02:38:11 -0600

    Implemented culling.

--- cull.c
+++ cull.c
@@ -16,6 +16,32 @@ int cull( Vertex *v1, Vertex *v2, Vertex *v3, Vertex *cameraPosition )
   //       normal vector.  Compute the dot product of these two
   //       vectors.  Return an indication of whether this triangle
   //       should be culled.
+
+  Vertex toTriangle, normal;
+
+  // To calculate the normal vector, perform the cross product of two vectors.
+  normal.x = v1->y * v2->z - v1->z * v2->y;
+  normal.y = v1->z * v2->x - v1->x * v2->z;
+  normal.z = v1->x * v2->y - v1->y * v2->x;
+
+  if (cameraPosition->z == 0)
+  {
+    cameraPosition->z = +HUGE_VAL;
+  }
+  
+  toTriangle.x = v1->x - cameraPosition->x;
+  toTriangle.y = v1->y - cameraPosition->y;
+  toTriangle.z = v1->z - cameraPosition->z;
+
+  if ( (normal.x * toTriangle.x + normal.y * toTriangle.y + normal.z * toTriangle.z) > 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+  
 }
 
 //----------------------------------------------------------
